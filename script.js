766c9c5d7169111d0ff6a5bf3612b29593ab2225
MacJ13 <jacdim02@gmail.com>
2023-02-14 21:10:30 +0100

    Add dom elements

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
+// Dom elements;
+const displayInput = document.querySelector('.display');
+const digitButtons = document.querySelectorAll('.digit');
+const operatorButtons = document.querySelectorAll('.operator');
+const equationButton = document.querySelector('.equation');
+const percentageButton = document.querySelector('.percentage');
+const plusminusButton = document.querySelector('.plusminus');
+const backspaceButton = document.querySelector('.backspace');
+const pointButton = document.querySelector('.point');
+const clearButton = document.querySelector('.clear');
+
+
+// addition function
 function add(a, b){
     return a + b;
 }
 
+
+// substraction function
 function substract(a, b){
     return a - b;
 }
 
+
+// multiplication function
 function multiply(a, b){
     return a * b;
 }
 
+// division function
 function divide(a , b){
     return a / b;
 }
 
+
+// create obj operator with mathematical function as values
 const calculation = {
     "+": add,
     '-': substract,
